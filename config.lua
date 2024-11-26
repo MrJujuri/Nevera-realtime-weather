93a04724fe49a0bf05a6d3d7e2a5e84189b27aad
! DevJujuri <zuhrihazim1970@gmail.com>
2024-11-27 01:22:55 +0800

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {
+    debug = true, -- Set to true to enable notifications, false to disable
+    disableFog = true, -- Set to true to disable fog weather
+    updateInterval = 60000, -- Interval for updating time and weather (ms)
+    weatherChangeInterval = 600000, -- Interval for requesting weather updates (ms)
+}
