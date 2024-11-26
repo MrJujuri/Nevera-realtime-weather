2c67baa4083c704f3ad3a0414c00b743914ff578
! DevJujuri <zuhrihazim1970@gmail.com>
2024-11-27 01:22:12 +0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Nevera - 2024'
-description 'Skripta za realno vrijeme - Europe/Zagreb (Split)'
-version '1.0.0'
+author 'Nevera - 2024 Edited By MrJujuri'
+description 'Script for real-time - Europe/Zagreb (Split)'
+version '1.0.1'
 
 server_script 'realtime_server.lua'
-client_script 'realtime_client.lua'
+client_script {
+    'realtime_client.lua',
+    'config.lua',
+}
